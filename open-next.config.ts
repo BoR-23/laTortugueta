6687f77cb604f197f3e52dad5367b62a7bafb7e9
BoR <losGarciaBros23@gmail.com>
2025-11-12 19:07:10 +0100

    Clean up project for Netlify deployment - remove Cloudflare dependencies and configs

--- open-next.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-  // @ts-ignore - Forzar configuración experimental de R2
-  cache: {
-    type: "r2",
-    binding: "CACHE_BUCKET",
-  },
-});
